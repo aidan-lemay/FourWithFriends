98596e1518b5418c27f30d322dfb51aa9ed539fc
Isaac Prusky <Isaac@MacBook-Air.local>
2020-04-20 14:10:47 -0400

    renamed file so it registered as an actual java file

--- BackendNET.java
+++ BackendNET.java
@@ -3,20 +3,20 @@ import java.io.*;
 
 /**
  * ISTE-121 final project backend multithreaded server code
- * 
+ *
  * @author Thomas Chenevey
  * @version 1.0
  * @date: 4/3/2020
  * @project: four with friends
  */
 
-public class BackEndNet {
+public class BackendNET {
 	public static void main(String [] args) {
-		new BackEndNet();
+		new BackendNET();
 	}
-	
-   //creating BackEndNet constructor
-	public BackEndNet()
+
+   //creating BackendNET constructor
+	public BackendNET()
 	{
 		ServerSocket ss = null;
 
@@ -28,15 +28,15 @@ public class BackEndNet {
 		  ss = new ServerSocket(16789);
 		  Socket cs = null;
         System.out.println("Server Started");
-        
+
         //after socket is created the server waits for a client to be accepted
         System.out.println("Waiting for a client...");
-		  while(true){ 		
+		  while(true){
 			 cs = ss.accept();
-          System.out.println("Client accepted"); 				
+          System.out.println("Client accepted");
 			 ThreadServer ts = new ThreadServer( cs );
 			 ts.start();
-		  } 
+		  }
 		}
 		catch( BindException be ) {
 			System.out.println("Server running, stopping");
@@ -46,8 +46,8 @@ public class BackEndNet {
 			ioe.printStackTrace();
 		}
 
-	} 
-	
+	}
+
    //Threadserver class
 	class ThreadServer extends Thread {
 		Socket cs;
@@ -55,29 +55,29 @@ public class BackEndNet {
 	public ThreadServer( Socket cs ) {
 		this.cs = cs;
 		}
-		
+
       //run method that handles receiving messages from client as well as sending messages back
 		public void run() {
 			BufferedReader br;
 			PrintWriter pw;
 			String clientMsg;
-			
+
          try {
 			  br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-                  
+
 			  pw = new PrintWriter(new OutputStreamWriter(cs.getOutputStream()));
-							
-			  clientMsg = br.readLine();					        
+
+			  clientMsg = br.readLine();
 			  System.out.println("Server read: "+ clientMsg);
-			  pw.println(clientMsg.toUpperCase());	
+			  pw.println(clientMsg.toUpperCase());
 			  pw.flush();
            }
-         
-			catch( IOException e ) { 
-				System.out.println("Inside catch"); 
+
+			catch( IOException e ) {
+				System.out.println("Inside catch");
 				e.printStackTrace();
 			}
-		
-         } //run method 
-	 } //ThreadServer 
-}//BackEndNet 
+
+         } //run method
+	 } //ThreadServer
+}//BackendNET
